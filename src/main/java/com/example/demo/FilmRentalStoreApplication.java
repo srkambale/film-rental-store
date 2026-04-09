4dcc3daf553ffbbe68bdadba58f2f93aa9537820
Megha Muttha <mutthamegha8@gmail.com>
2026-04-09 10:56:38 +0530

    Initial commit for payment service

---
+++ src/main/java/com/example/demo/FilmRentalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmRentalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmRentalStoreApplication.class, args);
+	}
+
+}
